4c1d60cb5e51cece41da8048abf0221fbf23bbe4
Alex Ben <adbenoit60@hotmail.com>
2019-03-03 00:40:18 -0500

    Ajouter une recettes à bd Firestore Recherche (Search) de recette par titre (Phrase exact)

--- app/src/main/java/com/example/andrespiraquive/recettes/AddRecipe.java
+++ app/src/main/java/com/example/andrespiraquive/recettes/AddRecipe.java
@@ -5,20 +5,90 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddRecipe extends AppCompatActivity {
+
+    private static final String TITRE_KEY = "titre";
+    private static final String INGREDIENT_KEY = "ingredient";
+    private static final String DESCRIPTION_KEY = "description";
+    private static final String PREPARATION_KEY = "preparation";
+    private static final String POSITION_KEY = "position";
+
+    FirebaseFirestore db;
+    EditText editTitre;
+    EditText editIngredient;
+    EditText editDescription;
+    EditText editPreparation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        db = FirebaseFirestore.getInstance();
+        editTitre = findViewById(R.id.titre);
+        editIngredient = findViewById(R.id.editIngredient);
+        editDescription = findViewById(R.id.editDescription);
+        editPreparation = findViewById(R.id.editPreparation);
+
+
+        final Button buttonSave = findViewById(R.id.saveRecipeButton);
+        final Button buttonCancel = findViewById(R.id.cancelRecipeButton);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                addNewRecipe();
+            }
+        });
+
+    }
+
+
+    private void addNewRecipe() {
+        Map<String, Object> newRecipe = new HashMap<>();
+        newRecipe.put(TITRE_KEY, editTitre.getText().toString());
+        newRecipe.put(INGREDIENT_KEY, editIngredient.getText().toString());
+        newRecipe.put(DESCRIPTION_KEY, editDescription.getText().toString());
+        newRecipe.put(PREPARATION_KEY, editPreparation.getText().toString());
+        newRecipe.put(POSITION_KEY, "45.462252,-73.437309");
+
+        db.collection("Recipes")
+                .add(newRecipe)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(AddRecipe.this, "Recipe Registered",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddRecipe.this, "ERROR" + e.toString(),
+                                Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", e.toString());
+                    }
+                });
     }
 
     @Override
--- app/src/main/java/com/example/andrespiraquive/recettes/SearchActivity.java
+++ app/src/main/java/com/example/andrespiraquive/recettes/SearchActivity.java
@@ -10,13 +10,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,45 +32,59 @@ public class SearchActivity extends AppCompatActivity {
 
     //private Button boutonRecherche;
 
+    private static final String TITRE_KEY = "titre";
+    private static final String INGREDIENT_KEY = "ingredient";
+    private static final String DESCRIPTION_KEY = "description";
+    private static final String PREPARATION_KEY = "preparation";
+    private static final String POSITION_KEY = "position";
+
+
+
+    FirebaseFirestore db;
+    TextView affichageRecette;
+    EditText searchLine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        /** FirebaseApp.initializeApp(this);
-          this.boutonRecherche = findViewById(R.id.searchButton);
+        db = FirebaseFirestore.getInstance();
+        affichageRecette = findViewById(R.id.result);
+        searchLine = findViewById(R.id.editSearch);
 
-        boutonRecherche.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-                // Create a new user with a first and last name
-                Map<String, Object> user = new HashMap<>();
-                user.put("first", "Ada");
-                user.put("last", "Lovelace");
-                user.put("born", 1815);
-
-                // Add a new document with a generated ID
-                db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                documentReference.getId();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-
-                            }
-                        });
-
+        final Button button = findViewById(R.id.searchButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                ReadRecipes();
             }
         });
-        */
+
+    }
+
+    private void ReadRecipes() {
+        db.collection("Recipes")
+        .whereEqualTo("titre", searchLine.getText().toString())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                StringBuilder fields = new StringBuilder("");
+                                fields.append("Titre: ").append(document.get(TITRE_KEY));
+                                fields.append("\nIngredient: ").append(document.get(INGREDIENT_KEY));
+                                fields.append("\nDescription: ").append(document.get(DESCRIPTION_KEY));
+                                fields.append("\nPreparation: ").append(document.get(PREPARATION_KEY));
+                                fields.append("\n\n");
+                                affichageRecette.append(fields.toString());
+                            }
+                        }else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     @Override
